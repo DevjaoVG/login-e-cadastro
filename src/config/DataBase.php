0343001f2ac3bc9f020030fcc83a4470d14ca3cf
João <Joo Vitor>
2025-10-28 18:51:50 -0300

    refactor: restruturando conexão com DB usando .env

--- src/config/DataBase.php
+++ src/config/DataBase.php
@@ -1,24 +1,37 @@
 <?php
 
-// Conexão com bando de dados
+require_once __DIR__ . "/../../vendor/autoload.php";
 
+use Dotenv\Dotenv;
+
+
+// Conexão com bando de dados
 class Database {
     private static $instance = null;
 
 
     private static function getConnection() {
         if (self::$instance == null) {
-            $host = "localhost";
-            $user = "root";
-            $password = "";
-            $db = "";
 
+            // Cria a instância do Dotenv apontando para a raiz do projeto
+            $dotenv = Dotenv::createImmutable(__DIR__ . "/../../");
+            $dotenv->load();
 
-            self::$instance = new mysqli($this->host, $this->user, $this->password, $this->db); 
+    
+            $db = $_ENV['DB_NAME'] ?? "meu_banco";
+            $host = $_ENV['DB_HOST'] ?? "localhost";
+            $user = $_ENV['DB_USER'] ?? "root";
+            $password = $_ENV['DB_PASS'] ?? "";
 
-            if ($this->conn->connect_error) {
-                die("Connection failed: " . $this->conn->connect_error);
+            self::$instance = new mysqli($host, $user, $password, $db); 
+
+            if (self::$instance->connect_error) {
+                die("Connection failed: " . $self::$instance->connect_error);
             }
+
+            
+            // Define charset para UTF-8
+            self::$instance->set_charset('utf8mb4');
         }
 
         return self::$instance;
