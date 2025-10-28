d49d17e4bfb736073f4a88a66d93e77ee1df715e
João <Joo Vitor>
2025-10-28 14:46:19 -0300

    feat: serviço de autenticação

---
+++ src/services/AuthService.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once __DIR__ . "/../model/User.php";
+
+
+class AuthService {
+    private $userRepo;
+
+
+    public function __construct(UserRepository $repo) {
+        $this->userRepo = $repo;
+    }
+
+
+    public function register($name, $email, $passaword) {
+        if ($this->userRepo->findByEmail($email)) {
+            throw new Exception("E-mail já cadastrado.")
+        }
+
+        $passawordHash = password_hash($password, PASSWORD_DEFAULT);
+        $user = new User($name, $email, $passawordHash);
+        $this->userRepo->save($user);
+    }
+
+
+    public function login($email, $passaword) {
+        $user = $this->userRepo->findByEmail($email);
+
+        if (!$user || !password_verify($passaword, $user->getPasswordHash())) {
+            throw new Exception("E-mail ou Senha invalidos.");
+        }
+
+        $_SESSION['user_id'] = $user->getId();
+        $_SESSION['user_name'] = $user->getName();
+
+        return $user;
+    }
+
+
+    public function logout() {
+        session_destroy();
+    }
+}
+
+
+?>
