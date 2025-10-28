42b6618786fae2f0dd8edf6290eddb05d159f2b8
João <Joo Vitor>
2025-10-28 13:20:08 -0300

    feat: modulo de usario

---
+++ src/model/User.php
@@ -0,0 +1,24 @@
+<?php
+
+class User {
+    private $id;
+    private $name;
+    private $email;
+    private $passwordHash;
+
+
+    public function __construct($id = null, $name, $email, $passwordHash) {
+        $this->$id = $id;
+        $this->$name = $name;
+        $this->$email = $email;
+        $this->$passwordHash = $passwordHash;
+    }
+
+    
+    public function getId() { return $this->$id };
+    public function getName() { return $this->$name };
+    public function getEmail() { return $this->$email };
+    public function getPasswordHash() { return $this->$passwordHash };
+}
+
+?>
